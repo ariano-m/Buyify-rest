492057732bf6e9b952930281ea7549e7fa3de156
J Portal <j.portal@outlook.com>
2020-03-03 18:59:27 +0100

    Return PDF file on request

--- src/main/java/com/buyify/rest/BuyifyRestApplicationController.java
+++ src/main/java/com/buyify/rest/BuyifyRestApplicationController.java
@@ -1,9 +1,13 @@
 package com.buyify.rest;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.List;
-
+import com.buyify.rest.entities.Order;
+import com.buyify.rest.entities.Product;
+import com.buyify.rest.repositories.OrderRepository;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,57 +18,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.buyify.rest.entities.Order;
-import com.buyify.rest.entities.Product;
-import com.buyify.rest.entities.User;
-import com.buyify.rest.repositories.OrderRepository;
-import com.buyify.rest.repositories.UserRepository;
-
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RestController
 public class BuyifyRestApplicationController {
-	
-	@Autowired
-	private OrderRepository orderRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
-	@RequestMapping(value = "/generarFactura/{username}", method = RequestMethod.GET, produces = "application/octet-stream")
-	public ResponseEntity<byte[]> generatePDF(@PathVariable String username) {
-		System.out.println("Servicio interno");
-		User user = userRepository.findByUsername(username);
-		List<Order> orders = orderRepository.findByUser(user);
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(user.getUsername()).append("\n");
-		
-		for(Order pedido : orders) {
-			stringBuilder.append(pedido.getId()).append(" " + pedido.getDate()).append("\n");
-			List<Product> products = pedido.getProducts();
-			
-			for(Product product : products) {
-				stringBuilder.append(product.getName() + " " + product.getPrice() + " " + product.getPromotion().getPromotion() + "\n");
-			}
-			
-			stringBuilder.append("\t");
-		}
-		
-		
-		try {
-			outputStream.write(String.valueOf(stringBuilder).getBytes());
-//			byte[] pdf = outputStream.toByteArray();
-			byte[] pdf = "esto es una prueba".getBytes();
-			return new ResponseEntity<byte[]>(pdf, HttpStatus.CREATED);
-			
-			
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-			
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @RequestMapping(value = "/generarFactura/{id}", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> generatePDF(@PathVariable long id) {
+        Optional<Order> order = orderRepository.findById(id);
+
+        String filename = "factura_" + id + ".pdf";
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        try {
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage(PDRectangle.A4);
+            document.addPage(page);
+
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+            contentStream.beginText();
+            contentStream.newLineAtOffset(10, 770);
+            contentStream.setFont(PDType1Font.TIMES_ROMAN, 16);
+            contentStream.showText(order.get().getProducts().stream().map(Product::getName).collect(Collectors.joining(" - ")));
+            contentStream.endText();
+
+            contentStream.close();
+
+            document.save(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.set("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+
+        return new ResponseEntity<>(output.toByteArray(), headers, HttpStatus.OK);
+    }
+
 }
